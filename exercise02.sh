acaa5bb19d2f92c9a6e904b09d3d3e4a67deaaa9
root <root@ip-172-31-10-250.ap-northeast-2.compute.internal>
2024-10-21 07:22:43 +0000

    Second Exercise

---
+++ exercise02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author: chakri sai charan t
+#Date: 21-10-2024
+#Purpose: The variable will hold the contents of the message “Shell Scripting is Fun!”
+#
+
+name="Shell Scripting is Fun!"
+echo $name
