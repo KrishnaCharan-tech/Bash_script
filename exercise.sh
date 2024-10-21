3b3a302bbfd56d29caae6312989825e63bdffddc
root <root@ip-172-31-10-250.ap-northeast-2.compute.internal>
2024-10-21 07:24:59 +0000

    First Exercise

---
+++ exercise.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Author: Chakri Sai Charan T
+#Date : 21-01-2024
+#Purpose: Write a shell script that prints “Shell Scripting is Fun!” on the screen
+#
+#
+echo "Shell Scripting is Fun!"
